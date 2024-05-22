d6df9cdd99258fb822d9fd84be3fffa36fd1d16d
Enor Cerna <u17200479@utp.edu.pe>
2024-05-22 03:08:08 -0500

    module component

---
+++ src/components/header/Search.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+  export let searchValue = "";
+</script>
+
+<script lang="ts">
+  import {onMount} from "svelte";
+  import Input from "../ui/input";
+  let value = "";
+  onMount(() => {
+    searchValue = value;
+  });
+</script>
+
+<aside>
+  <Input bind:value />
+</aside>
